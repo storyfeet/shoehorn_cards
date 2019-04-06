25b2f4d6169d125e1e7eaaee14f65b2ad7559374
storyfeet <thebeaky@psmail.net>
2019-04-06 12:41:13 +0100

    Flavour text is now a thing

--- src/card_front.rs
+++ src/card_front.rs
@@ -1,6 +1,5 @@
 use crate::card_type::CardType;
 use lazy_conf::{Getable, Lz};
-//use mksvg::text::wrap_nl;
 use mksvg::{text, Card, SvgArg, SvgWrite, Tag, Text};
 
 #[derive(Debug, Clone)]
@@ -15,6 +14,7 @@ pub struct CardFront {
     pub tp: CardType,
     pub tx: String,
     pub gl: Vec<String>,
+    pub extra: Option<String>,
 }
 
 impl CardFront {
@@ -26,12 +26,14 @@ impl CardFront {
                 tp,
                 tx: l.get("tx").unwrap_or("".to_string()),
                 gl: l.get_array("gl", 10),
+                extra: l.get("extra"),
             },
             _ => CardFront {
                 name: l.name.clone(),
                 tp,
                 tx: mksvg::text::escapes(&l.get("tx").ok_or(CardErr::NoText)?),
                 gl: Vec::new(),
+                extra: l.get("extra"),
             },
         })
     }
@@ -51,14 +53,17 @@ impl Card<f64> for CardFront {
         } else {
             (10, h / 11.)
         };
-        let nname = text::wrap(&self.name, wrap);
 
+        //Card Name
+        let nname = text::wrap(&self.name, wrap);
         Text::lines(&nname, w / 2., h / 8., fsize)
             .fill("black")
             .font_size(fsize)
             .text_anchor("middle")
             .font_weight("bold")
             .write(s);
+
+        //Card Text
         let ttx = match self.tp {
             CardType::Goal | CardType::Role => {
                 let mut n = 0;
@@ -73,15 +78,23 @@ impl Card<f64> for CardFront {
             _ => mksvg::text::wrap(&self.tx, 21),
         };
 
-        Text::lines(ttx, w / 2., h * 0.55, h / 15.)
-            .v_center()
-            .fill("black")
-            .text_anchor("middle")
-            .font_size(h / 18.)
-            .write(s);
+        let mut wa = |x: Text<_>| {
+            x.fill("black")
+                .text_anchor("middle")
+                .font_size(h / 18.)
+                .write(s)
+        };
 
+        wa(Text::lines(ttx, w / 2., h * 0.3, h / 15.));
+
+        if let Some(ref ex) = self.extra {
+            wa(Text::lines(text::wrap(ex, 20), w / 2., h * 0.8, h / 15.)
+                .font_weight("bold")
+                .v_base());
+        }
+
+        //Card Type
         let col = self.tp.color();
-
         Tag::rect(0., h * 0.8, w, h * 0.1).fill(col).write(s);
 
         Text::new(&self.tp.to_string(), w / 2., h * 0.88, h * 0.08)
